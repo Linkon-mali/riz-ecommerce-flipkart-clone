19ef51e2481d868a0196a806791c5f89f396e9c6
Linkon mali <linkonmali.cse@gmail.com>
2022-09-17 00:38:40 +0600

    first commit

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,4 +1,13 @@
-export const api = 'http://localhost:2000/api';
-export const generatePublicUrl = (filename) => {
-    return `http://localhost:2000/public/${filename}`;
-}
+// export const api = 'http://localhost:2000/api';
+// export const generatePublicUrl = (filename) => {
+//     return `http://localhost:2000/public/${filename}`;
+// }
+
+const baseUrl = "https://riz-ecommerce-mern-back-end.herokuapp.com";
+//const baseUrl = "http://localhost:2000";
+
+export const api = `${baseUrl}/api`;
+
+export const generatePublicUrl = (fileName) => {
+  return `${baseUrl}/public/${fileName}`;
+};
